cb773dc0405de28b5f81555eae577f553d51a9c3
yoneyama ryo <uryonym@outlook.com>
2021-12-30 14:38:16 +0900

    setup eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -14,11 +18,9 @@ module.exports = {
     },
     ecmaVersion: 13,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
-};
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+}
