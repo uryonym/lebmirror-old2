6a606376184466a0ac42b08b335e2a9102aa1923
yoneyama ryo <uryonym@outlook.com>
2021-12-31 07:02:46 +0900

    install chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
@@ -6,7 +7,9 @@ import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
   document.getElementById('root')
 )
